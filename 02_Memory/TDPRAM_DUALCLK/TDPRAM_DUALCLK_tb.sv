f80c306759cfaafb042b1fe0ea900561987e59c0
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-12-29 22:59:00 +0900

    Update TDPRAM_DUALCLK_tb.sv

--- 02_Memory/TDPRAM_DUALCLK/TDPRAM_DUALCLK_tb.sv
+++ 02_Memory/TDPRAM_DUALCLK/TDPRAM_DUALCLK_tb.sv
@@ -9,7 +9,7 @@
 *
 * License under CERN-OHL-P v2
 --------------------------------------------------------------------------------
-| Copyright AUDIY 2025.                                                        |
+| Copyright AUDIY 2025 - 2026.                                                 |
 |                                                                              |
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
@@ -203,3 +203,4 @@ module TDPRAM_DUALCLK_tb;
 endmodule
 
 `default_nettype wire
+
