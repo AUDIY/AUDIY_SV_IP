f3d96be5acb7b04d03132913788d72559de68bb8
AUDIY <96096729+AUDIY@users.noreply.github.com>
2025-12-29 22:58:00 +0900

    Update SPROM_tb.sv

--- 02_Memory/SPROM/SPROM_tb.sv
+++ 02_Memory/SPROM/SPROM_tb.sv
@@ -9,7 +9,7 @@
 *
 * License under CERN-OHL-P v2
 --------------------------------------------------------------------------------
-| Copyright AUDIY 2025.                                                        |
+| Copyright AUDIY 2025 - 2026.                                                 |
 |                                                                              |
 | This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
 |                                                                              |
@@ -105,3 +105,4 @@ module SPROM_tb ();
 endmodule
 
 `default_nettype wire
+
